def8aac9902796a1d372f521fc78b442f4720434
gautierlsn <gautierluisin@gmail.com>
2022-03-28 21:20:54 +0200

    Modification style page de login et register

--- Script_PHP/function_general.php
+++ Script_PHP/function_general.php
@@ -10,13 +10,11 @@ function makeHead($css,$jquery,$scriptjs){
     <meta charset = "UTF-8" >
     <meta name ="viewport" content = "user-scalable=0, width=device-width, maximum-scale=1.0, initial-scale=1.0">
     <link rel="stylesheet" href="'.$css.'">
-    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/font-awesome.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
     <script src="//cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
     <script src ="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity ="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin ="anonymous"></script>
     <script src="'.$jquery.'"></script>
     <script src="'.$scriptjs.'"></script>
-    <script src ="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity ="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin ="anonymous"></script>
 </head>
 <header id="home">
 </header>
@@ -79,7 +77,7 @@ echo '
 function register_form($msg_error){
     echo '
         <body class="bg">
-            <main class="login-form h-100">
+            <main class="register-form h-100">
                 <div class="container h-100">
                     <div class="row h-100">
                         <div class="col-md-12 my-auto">
