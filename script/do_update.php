614c5daadb30f6d1210c0ae554d866bfa2770784
gautierlsn <gautierluisin@gmail.com>
2022-04-23 14:56:15 +0200

    Page modification du profile

---
+++ script/do_update.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+
+    require("../generic/function_general.php");
+    require("../generic/function_bdd.php");
+
+    $action = $_GET['action'];
+
+    if($action == "updateProfile"){
+        $nom = checkInput($_POST['nom']);
+        $prenom = checkInput($_POST['prenom']);
+        $dateNaiss = checkInput($_POST['dateNaiss']);
+        $email = checkInput($_POST['email']);
+        $mdp = checkInput($_POST['mdp']);
+
+        //Modification du profil
+        doUpdateProfile($nom,$prenom,$dateNaiss,$email,$mdp,$_SESSION['id']);
+
+        header("Location: ../web_page/profile.php");
+    }
+?>
